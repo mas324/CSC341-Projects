b22114bad6e43701a60c43c2a79992e5caff339c
Shawn <shawnmerana@gmail.com>
2023-02-14 20:22:16 -0800

    Added more stuff

---
+++ feb14.c
@@ -0,0 +1,26 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    pid_t pid;
+
+    while (1)
+    {
+    pid = fork();
+
+    if (pid < 0) {
+        fprintf(stderr, "Fork Failed");
+        return 1;
+    }
+    else if (pid == 0) {
+        fprintf(stdout, "I am the main");
+    }
+    else {
+        wait(NULL);
+        printf("Child Complete");
+    }
+    }
+
+    return 0;
+}
