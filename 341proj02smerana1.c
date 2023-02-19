913570bf28f2448f628145d227001987b3d1db3c
Shawn <20176147+mas324@users.noreply.github.com>
2023-02-19 03:00:36 -0800

    Added argument analyzer

--- 341proj02smerana1.c
+++ 341proj02smerana1.c
@@ -3,32 +3,49 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
 
 int main (int argc, char* argv[]) {
     DIR *dirp;
     struct direct *dp;
     struct stat buf;
     char* psDir = ".";
+    int exTime = 0;
+    int exFile = 0;
 
     if (argc > 1) {
-
-        for (int i = 0; i < argc; i++)
-        {
-            printf("%s", argv[i]);
-        }
-        
-        if (argc > 2){
-            if (argv[2] == "a") {
-                
+        if ((argc == 2) && (argv[1][0] != '-')) {
+            psDir = argv[1];
+        } else {
+            for (int i = 1; i < argc; i++) {
+                if (argv[i][0] == '-') {
+                    if (argv[i][1] == 'l') {
+                        exTime++;
+                    } else if (argv[i][1] == 'a') {
+                        exFile++;
+                    } else {
+                        printf("usage: ls [-l|-a] location");
+                        return 1;
+                    }
+                }
             }
         }
-        return 1;
     }
 
     dirp = opendir(psDir);
     for (dp = readdir(dirp); dp != NULL; dp = readdir(dirp)) {
         stat(dp->d_name, &buf);
-        printf("%s\t%s", dp->d_name, ctime(&(buf.st_mtime)));
+
+        if (!exFile && (dp->d_name[0] == '.')) {
+            continue;
+        } 
+
+        if (exTime) {
+            printf("%s\t%s", dp->d_name, ctime(&(buf.st_mtime)));
+        } else {
+            printf("%s\n", dp->d_name);
+        }
     }
     closedir(dirp);
+    return 0;
 }
---
+++ Test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char argv[])
+{
+    char* x = "12345678";
+    printf("%lu", sizeof(x));
+}
