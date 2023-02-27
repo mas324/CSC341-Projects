51944d3bf6b89eacf07c8be9fd074fb258d61263
Shawn <20176147+mas324@users.noreply.github.com>
2023-02-26 23:14:11 -0800

    Comments and log

--- 341proj02smerana1.c
+++ 341proj02smerana1.c
@@ -10,17 +10,19 @@ int main (int argc, char* argv[]) {
     struct direct *dp;
     struct stat buf;
     char* psDir = ".";
-    int exTime = 0;
-    int exFile = 0;
+    int exTime = 0; //boolean for if -l argument was passed
+    int exFile = 0; //boolean for if -a argument was passed
 
-    if (argc > 1) {
+    if (argc > 1) { //if no args then just return current directory info
         if ((argc == 2) && (argv[1][0] != '-')) {
             psDir = argv[1];
         } else if (!strcmp(argv[1], "-la") || !strcmp(argv[1], "-al")) {
                 exTime = 1;
                 exFile = 1;
-                if (argc == 3) {
+                if (argc == 3) { //check to ensure arguments match
                     psDir = argv[2];
+                } else {
+                    printf("usage: ls [-l|-a] location\n");
                 }
         } else if (argc <= 4) {
             int i;
@@ -50,7 +52,7 @@ int main (int argc, char* argv[]) {
 
     dirp = opendir(psDir);
 
-    if (dirp == NULL) {
+    if (dirp == NULL) { //if the given path is a file then parse as file
         stat(psDir, &buf);
         if (exTime) {
             printf("%s\t%s", psDir, ctime(&(buf.st_mtime)));
