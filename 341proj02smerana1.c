36c6d6a98ea9fcabb0e7e1eab6b474983e2339ae
Shawn <20176147+mas324@users.noreply.github.com>
2023-03-07 19:48:34 -0800

    fixes again

--- 341proj02smerana1.c
+++ 341proj02smerana1.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main (int argc, char* argv[]) {
     DIR *dirp;
@@ -37,7 +38,7 @@ int main (int argc, char* argv[]) {
                             break;
                         default:
                             printf("usage: ls [-l|-a] location\n");
-                            return 1;
+                            exit(1);
                     }
                 } else {
                     psDir = argv[i];
@@ -46,12 +47,11 @@ int main (int argc, char* argv[]) {
             }
         } else {
             printf("usage: ls [-l|-a] location\n");
-            return 1;
+            exit(1);
         }
     }
 
     dirp = opendir(psDir);
-
     if (dirp == NULL) { //if the given path is a file then parse as file
         stat(psDir, &buf);
         if (exTime) {
@@ -62,10 +62,9 @@ int main (int argc, char* argv[]) {
     } else {
         for (dp = readdir(dirp); dp != NULL; dp = readdir(dirp)) {
             stat(dp->d_name, &buf);
-
             if (!exFile && (dp->d_name[0] == '.')) {
                 continue;
-            } 
+            }
 
             if (exTime) {
                 printf("%s\t%s", dp->d_name, ctime(&(buf.st_mtime)));
@@ -75,5 +74,5 @@ int main (int argc, char* argv[]) {
         }
     }
     closedir(dirp);
-    return 0;
+    exit(0);
 }
