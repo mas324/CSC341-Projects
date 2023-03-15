93cd67fb6dc4a9b4ae3a06005b56dbb58f529762
Shawn <20176147+mas324@users.noreply.github.com>
2023-03-14 20:16:18 -0700

    lesson mar14

---
+++ 14Mar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int main(int argc, char const *argv[]) {
+    /* code */
+    return 0;
+}
+
+void fun1() {
+
+}
+
+void fun2() {
+    
+}
--- Test.c
+++ Test.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <threads.h>
 
 int main(int argc, char* argv[])
 {
     __pid_t pid;
     pid = fork();
-
-    if (pid == 0) {
-        char* vector[] = {"ls", "-la", "/home", NULL};
-        execvp(vector[0], vector);
-        perror("Errored out");
-    } else if (pid < 0) {
-        printf("Something went wrong\n");
-    } else {
-        sleep(2);
+    if (pid == 0)
+    {
+        fork();
+        printf("Hello there I am %d", getpid());
+        printf("Thread");
     }
-    printf("This is the file end for pid=%d\n", getpid());
+    fork();
 }
